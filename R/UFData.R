fa50528cbfa4998819c696dc075418a1dd3f0b13
Giovanni dos Santos Vargette <160137250+GiovanniVargette@users.noreply.github.com>
2024-03-08 11:09:54 -0300

    UFData

---
+++ R/UFData.R
@@ -0,0 +1,23 @@
+#' UFData Function
+#'
+#'The UFData function filters the data based on the name of the Brazilian
+#'Federative Unit provided.
+#'
+#' @example SP = UFData("São Paulo")
+#'
+#'
+#'
+#' @export
+#'
+#'
+#' @param Estado Use the UF's name to select the one you want.
+#'
+
+
+UFData <- function (Estado) {
+  openxlsx::read.xlsx("http://dados.mj.gov.br/dataset/210b9ae2-21fc-4986-89c6-2006eb4db247/resource/feeae05e-faba-406c-8a4a-512aec91a9d1/download/indicadoressegurancapublicauf.xlsx") |>
+    tidyr::pivot_wider(
+      names_from = Tipo.Crime, #As colunas
+      values_from = Ocorrências) |>  #Os valores
+    dplyr::filter( UF == Estado)
+}
