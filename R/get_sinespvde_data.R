cacb01cb8043790b4a13b312c2b8972b9f989f9c
Giovanni dos Santos Vargette <160137250+GiovanniVargette@users.noreply.github.com>
2024-04-16 12:17:07 -0300

    get_sinespvde_data_V6
    Nova abordagem para a coleta dos dados. Eles são baixados previamente, e acessados através de um load().
    Implementação inicial do filtro de granularidade anual, falta terminar algumas categorias

--- R/get_sinespvde_data.R
+++ R/get_sinespvde_data.R
@@ -16,156 +16,7 @@
 get_sinespvde_data <- function(state = 'all', city = "all", category = "all", typology = 'all',
                                 year = 'all', granularity = 'month', pivot = F, geom = F) {
 
-  options(scipen = 999, timeout = 1500)
-
-  link19 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2019.xlsx/"
-
-  link20 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2020.xlsx/"
-
-  link21 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2021.xlsx/"
-
-  link22 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2022.xlsx/"
-
-  link23 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2023.xlsx/"
-
-  link24 <- "https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/banco-vde-2024.xlsx/"
-
-
-
-  #ufs <- read.csv("BrazilCrime/data-raw/ufs.csv")
-
-  suppressWarnings({
-    # download and initial data treatment
-    tryCatch({
-
-      df19 <- openxlsx::read.xlsx(link19) |>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 43466 ~ 1,
-          data_referencia == 43497 ~ 2,
-          data_referencia == 43525 ~ 3,
-          data_referencia == 43556 ~ 4,
-          data_referencia == 43586 ~ 5,
-          data_referencia == 43617 ~ 6,
-          data_referencia == 43647 ~ 7,
-          data_referencia == 43678 ~ 8,
-          data_referencia == 43709 ~ 9,
-          data_referencia == 43739 ~ 10,
-          data_referencia == 43770 ~ 11,
-          data_referencia == 43800 ~ 12))|>
-        dplyr::mutate(ano=2019)
-
-      df20 <- openxlsx::read.xlsx(link20) |>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 43831 ~ 1,
-          data_referencia == 43862 ~ 2,
-          data_referencia == 43891 ~ 3,
-          data_referencia == 43922 ~ 4,
-          data_referencia == 43952 ~ 5,
-          data_referencia == 43983 ~ 6,
-          data_referencia == 44013 ~ 7,
-          data_referencia == 44044 ~ 8,
-          data_referencia == 44075 ~ 9,
-          data_referencia == 44105 ~ 10,
-          data_referencia == 44136 ~ 11,
-          data_referencia == 44166 ~ 12))|>
-        dplyr::mutate(ano=2020)
-
-      df21 <- openxlsx::read.xlsx(link21) |>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 44197 ~ 1,
-          data_referencia == 44228 ~ 2,
-          data_referencia == 44256 ~ 3,
-          data_referencia == 44287 ~ 4,
-          data_referencia == 44317 ~ 5,
-          data_referencia == 44348 ~ 6,
-          data_referencia == 44378 ~ 7,
-          data_referencia == 44409 ~ 8,
-          data_referencia == 44440 ~ 9,
-          data_referencia == 44470 ~ 10,
-          data_referencia == 44501 ~ 11,
-          data_referencia == 44531 ~ 12))|>
-        dplyr::mutate(ano=2021)
-
-      df22 <- openxlsx::read.xlsx(link22)|>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 44562 ~ 1,
-          data_referencia == 44593 ~ 2,
-          data_referencia == 44621 ~ 3,
-          data_referencia == 44652 ~ 4,
-          data_referencia == 44682 ~ 5,
-          data_referencia == 44713 ~ 6,
-          data_referencia == 44743 ~ 7,
-          data_referencia == 44774 ~ 8,
-          data_referencia == 44805 ~ 9,
-          data_referencia == 44835 ~ 10,
-          data_referencia == 44866 ~ 11,
-          data_referencia == 44896 ~ 12))|>
-        dplyr::mutate(ano=2022)
-
-      df23 <- openxlsx::read.xlsx(link23)|>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 44927 ~ 1,
-          data_referencia == 44958 ~ 2,
-          data_referencia == 44986 ~ 3,
-          data_referencia == 45017 ~ 4,
-          data_referencia == 45047 ~ 5,
-          data_referencia == 45078 ~ 6,
-          data_referencia == 45108 ~ 7,
-          data_referencia == 45139 ~ 8,
-          data_referencia == 45170 ~ 9,
-          data_referencia == 45200 ~ 10,
-          data_referencia == 45231 ~ 11,
-          data_referencia == 45261 ~ 12))|>
-        dplyr::mutate(ano=2023)
-
-      df24 <- openxlsx::read.xlsx(link24)|>
-        dplyr::mutate(mes = dplyr::case_when(
-          data_referencia == 45292 ~ 1,
-          data_referencia == 45323 ~ 2))|>
-        dplyr::mutate(ano=2024)
-
-      df <- dplyr::bind_rows(df19,df20,df21,df22,df23,df24) |>
-        dplyr::mutate(categoria = dplyr::case_when(
-          evento == "Apreensão de Cocaína" ~ "drogas",
-          evento == "Apreensão de Maconha" ~ "drogas",
-          evento == "Tráfico de drogas" ~ "drogas",
-          evento == "Arma de Fogo Apreendida" ~ "arma de fogo",
-          evento == "Feminicídio" ~ "vitimas",
-          evento == "Homicídio doloso" ~ "vitimas",
-          evento == "Lesão corporal seguida de morte" ~ "vitimas",
-          evento == "Morte no trânsito ou em decorrência dele (exceto homicídio doloso)" ~ "vitimas",
-          evento == "Mortes a esclarecer (sem indício de crime)" ~ "vitimas",
-          evento == "Roubo seguido de morte (latrocínio)" ~ "vitimas",
-          evento == "Suicídio" ~ "vitimas",
-          evento == "Tentativa de homicídio" ~ "vitimas",
-          evento == "Estupro" ~ "vitimas",
-          evento == "Morte por intervenção de Agente do Estado" ~ "vitimas",
-          evento == "Furto de veículo" ~ "ocorrencias",
-          evento == "Roubo a instituição financeira" ~ "ocorrencias",
-          evento == "Roubo de carga" ~ "ocorrencias",
-          evento == "Roubo de veículo" ~ "ocorrencias",
-          evento == "Pessoa Desaparecida" ~ "desaparecidos/localizados",
-          evento == "Pessoa Localizada" ~ "desaparecidos/localizados",
-          evento == "Mandado de prisão cumprido" ~ "mandado de prisao cumprido",
-          evento == "Morte de Agente do Estado" ~ "profissionais de seguranca",
-          evento == "Suicídio de Agente do Estado" ~ "profissionais de seguranca",
-          evento == "Atendimento pré-hospitalar" ~ "bombeiros",
-          evento == "Busca e salvamento" ~ "bombeiros",
-          evento == "Combate a incêndios" ~ "bombeiros",
-          evento == "Emissão de Alvarás de licença" ~ "bombeiros",
-          evento == "Realização de vistorias" ~ "bombeiros")) |>
-        dplyr::select(uf,municipio,ano,mes,categoria,evento,agente,arma,faixa_etaria,feminino,
-                      masculino,nao_informado,total,total_peso,total_vitimas) |>
-        dplyr::arrange(uf,municipio,ano,mes,categoria,evento)
-
-      message("Download completed.")
-
-    }, error = function(e1) {
-      message("Error downloading file. Try again later.")
-
-    })
-  })
-
+  load("data/df.rda")
 
   # Function arguments ---------------------------------------------------------
 
@@ -206,7 +57,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                   nao_informado,total_vitimas) |>
                   dplyr::filter({categoria} %in% {{category}}) |>
                   dplyr::filter({evento} %in% {{typology}}),
-              stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+              stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "drogas"){
     ifelse(typology=="all",
@@ -220,19 +72,21 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                                   nao_informado,total_vitimas) |>
                   dplyr::filter({categoria} %in% {{category}}) |>
                   dplyr::filter({evento} %in% {{typology}}),
-                stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "arma de fogo"){
     ifelse(typology=="all",
            df <- df |>
              dplyr::select(uf,municipio,ano,mes,categoria,evento,arma,total) |>
-             dplyr::filter({categoia} %in% {{category}}),
+             dplyr::filter({categoria} %in% {{category}}),
            ifelse(typology == "Arma de Fogo Apreendida",
                   df <- df |>
                     dplyr::select(uf,municipio,ano,mes,categoria,evento,arma,total) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "ocorrencias"){
     ifelse(typology=="all",
@@ -245,7 +99,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                     dplyr::select(uf,municipio,ano,mes,categoria,evento,total) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "desaparecidos/localizados"){
     ifelse(typology=="all",
@@ -259,7 +114,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                                   masculino,nao_informado,total_vitimas) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "mandado de prisao cumprido"){
     ifelse(typology=="all",
@@ -271,7 +127,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                     dplyr::select(uf,municipio,ano,mes,categoria,evento,total) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "profissionais de seguranca"){
     ifelse(typology=="all",
@@ -285,7 +142,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                            masculino,nao_informado,total_vitimas) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
   if(category == "bombeiros"){
     ifelse(typology=="all",
@@ -299,7 +157,8 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
                     dplyr::select(uf,municipio,ano,mes,categoria,evento,total) |>
                     dplyr::filter({categoria} %in% {{category}}) |>
                     dplyr::filter({evento} %in% {{typology}}),
-                  stop("A tipologia introduzida não corresponde à categoria fornecida")))}
+                  stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
 
     # argument year
     if (!"all" %in% year) {
@@ -308,11 +167,74 @@ get_sinespvde_data <- function(state = 'all', city = "all", category = "all", ty
     }
 
     # argument granularity
-    if (granularity != 'month' & granularity == 'year') {
-      df <- df |>
-        dplyr::group_by(uf, evento, ano) |>
-        dplyr::summarise(total = sum(total, na.rm = T))
+    if (granularity == 'year' & category == "all") {
+      stop("Para utilizar a granularidade anual, selecione previamente uma categoria")
     }
+
+    if (granularity=="year" & category == "vitimas"){
+      ifelse(typology=="all",
+             df <- df |>
+               dplyr::group_by(uf,evento,ano)|>
+               dplyr::summarize(total_fem = (sum(feminino, na.rm = T)),
+                                total_masc = (sum(masculino, na.rm = T)),
+                                total_n_inf = (sum(nao_informado, na.rm = T)),
+                                total = sum(total_vitimas, na.rm = T)),
+             ifelse(typology == "Feminicídio" | typology == "Homicídio doloso" |
+                      typology == "Lesão corporal seguida de morte" |
+                      typology == "Morte no trânsito ou em decorrência dele (exceto homicídio doloso)" |
+                      typology == "Mortes a esclarecer (sem indício de crime)" | typology == "Roubo seguido de morte (latrocínio)" |
+                      typology == "Suicídio" | typology == "Tentativa de homicídio" | typology == "Estupro" |
+                      typology == "Morte por intervenção de Agente do Estado",
+                    df <- df |>
+                      dplyr::group_by(uf,{{typology}},ano) |>
+                      dplyr::summarize(total_fem = (sum(feminino, na.rm = T)),
+                                       total_masc = (sum(masculino, na.rm = T)),
+                                       total_n_inf = (sum(nao_informado, na.rm = T)),
+                                       total = sum(total_vitimas, na.rm = T)),
+                    stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
+
+    if (granularity=="year" & category == "ocorrencias"){
+      ifelse(typology=="all",
+             df <- df |>
+               dplyr::group_by(uf,evento,ano)|>
+               dplyr::summarize(total = sum(total, na.rm = T)),
+             ifelse(typology == "Furto de veículo" | typology == "Roubo a instituição financeira" |
+                      typology == "Roubo de carga" | typology == "Roubo de veículo",
+                    df <- df |>
+                      dplyr::group_by(uf,{{typology}},ano) |>
+                      dplyr::summarize(total = sum(total, na.rm = T)),
+                    stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
+
+    if (granularity=="year" & category == "drogas"){
+      ifelse(typology=="all",
+             df <- df |>
+               dplyr::group_by(uf,evento,ano)|>
+               dplyr::summarize(total_casos = sum(total),
+                                total_peso = sum(total_peso)),
+             ifelse(typology == "Apreensão de Cocaína" | typology == "Apreensão de Maconha" |
+                    typology == "Tráfico de drogas",
+                    df <- df |>
+                      dplyr::group_by(uf,{{typology}},ano) |>
+                      dplyr::summarize(total_casos = sum(total),
+                                       total_peso = sum(total_peso)),
+                    stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
+
+    if (granularity=="year" & category == "arma de fogo"){
+      ifelse(typology=="all",
+             df <- df |>
+               dplyr::group_by(uf,evento,ano,arma)|>
+               dplyr::summarize(total = sum(total)),
+             ifelse(typology == "Arma de Fogo Apreendida",
+                    df <- df |>
+                      dplyr::group_by(uf,evento,ano,arma) |>
+                      dplyr::summarize(total),
+                    stop("A tipologia introduzida não corresponde à categoria fornecida")))
+    }
+
+
 
     # argument relative_values
     #if (relative_values == T & granularity == 'month') {
